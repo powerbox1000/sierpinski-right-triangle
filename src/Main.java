1ed6abe43c8360872a17300dddcd69cf9d9e0046
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2024-07-10 12:49:54 -0400

    Implement "swap files"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,74 @@
 import java.util.Scanner;
 import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+
+class Swappable {
+  private boolean[] loadedRow;
+  private int currRow = 0;
+
+  public Swappable(int size) throws Exception {
+    loadedRow = new boolean[size];
+
+    for (int row = 0; row < size; row++){
+      FileWriter writer = new FileWriter("swap/swap_" + row);
+      for (int col = 0; col < size; col++){
+        writer.write('0');
+      }
+      writer.close();
+    }
+
+    loadRow(0, false);
+
+    System.out.println("Swap files generated.");
+  }
+
+  public int length(){
+    return loadedRow.length;
+  }
+
+  private void loadRow(int row) throws Exception {
+    loadRow(row, true);
+  }
+
+  private void loadRow(int row, boolean unload) throws Exception {
+    if(unload) unloadRow();
+    currRow = row;
+    File currswap = new File("swap/swap_" + row);
+    Scanner reader = new Scanner(currswap);
+    char[] data = reader.nextLine().toCharArray();
+    reader.close();
+    for (int i = 0; i < data.length; i++){
+      loadedRow[i] = (data[i] == '1');
+    }
+  }
+
+  private void unloadRow() throws Exception {
+    FileWriter writer = new FileWriter("swap/swap_" + currRow);
+    for (int col = 0; col < loadedRow.length; col++){
+      writer.write(loadedRow[col] ? '1' : '0');
+    }
+    writer.close();
+  }
+
+  public boolean get(int row, int col) throws Exception {
+    if (row != currRow) loadRow(row);
+
+    return loadedRow[col];
+  }
+
+  public void set(int row, int col, boolean val) throws Exception {
+    if (row != currRow) loadRow(row);
+
+    loadedRow[col] = val;
+  }
+
+  public void cleanSwap() throws Exception {
+    for(File file: (new File("swap")).listFiles()) 
+    if (!file.isDirectory()) 
+        file.delete();
+  }
+}
 
 public class Main {
   public static void main(String[] args) throws Exception {
@@ -9,7 +77,6 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter recurse amount: ");
     final int recurse = scanner.nextInt();
-    //if (recurse > 15) throw new Exception("Summertech has banned crashing computers sorry :(");
     final int numShapes = (int) Math.pow(3, recurse-1);
     scanner.close();
     if (recurse <= 0) {
@@ -19,21 +86,21 @@ public class Main {
 
     final int rows = (int) Math.pow(2, recurse - 1);
 
-    final boolean[][] shape;
+    final Swappable shape;
     if (recurse > 1){
-      shape = buildShape(new boolean[rows][rows], recurse, numShapes, new int[0]);
+      shape = buildShape(new Swappable(rows), recurse, numShapes, new int[0]);
     } else {
-      shape = new boolean[1][1];
-      shape[0][0] = true;
+      shape = new Swappable(1);
+      shape.set(0, 0, true);
     }
     
     FileWriter ihatethis = new FileWriter("triangle.txt");
 
-    for (int row = 0; row < shape.length; row++) {
+    for (int row = 0; row < shape.length(); row++) {
       final int len = (row + 1);
       for (int col = 0; col < len * 2; col++) {
         final int i = col >= len ? col - len : col;
-        if (shape[row][i]) {
+        if (shape.get(row, i)) {
           if (col < len) {
             if (col + 1 == len){
               ihatethis.write("*");
@@ -49,28 +116,21 @@ public class Main {
         if (i == len - 1)
           ihatethis.write('\n');
       }
-      System.out.println("Finished row " + (row + 1) + " out of " + shape.length);
+      System.out.println("Finished row " + (row + 1) + " out of " + shape.length());
     }
     ihatethis.close();
 
     System.out.println("Finished in " + (System.currentTimeMillis() - startTime) + " ms");
+    shape.cleanSwap();
     System.exit(0);
   }
 
-  public static int addArray(final int[] num){
-    int x = 0;
-    for (int i: num){
-      x += i;
-    }
-    return x;
-  }
-
-  public static boolean[][] buildShape(
-    boolean[][] shape,
+  public static Swappable buildShape(
+    Swappable shape,
     int recurse,
     int numShapes,
     int[] boxids
-  ){
+  ) throws Exception {
     if (recurse > 1){
       // recurse
       for (int i = 0; i < 3; i++){
@@ -96,7 +156,9 @@ public class Main {
       final int x = Integer.parseInt(firstBit, 2);
       final int y = Integer.parseInt(secondBit, 2);
 
-      shape[y][x] = true;
+      System.out.println("Drawing (" + x + ", " + y + ")");
+
+      shape.set(y, x, true);
     }
     
     return shape;
